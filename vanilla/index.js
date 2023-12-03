4834cc3295e7800abb9360773f5c48bc6a9318a0
lakshay deep <deep.lakshya1310@gmail.com>
2023-12-03 19:04:48 +0530

    practice

---
+++ vanilla/index.js
@@ -0,0 +1,56 @@
+const students = [
+    {
+        name: "lakshay",
+        age: 22,
+    },
+    {
+        name: "naman",
+        age: 12,
+    },
+    {
+        name: "mynk",
+        age: 16,
+    }, 
+    {
+        name: "hersh",
+        age: 40,
+    }, 
+    {
+        name: "shek",
+        age: 36,
+    }, 
+]
+// const above_18 =  students.filter((item) => item.age>18)
+// // console.log(above_18);
+
+// const container = document.getElementById('container')
+
+
+// above_18.forEach((item, count) => {
+//     // get div 
+//     const div = document.createElement('div')
+//         div.innerHTML = item.name;
+//         container.appendChild(div)
+    
+//     // put each element as div's value
+//     // append it to the main div
+// })
+
+// const inputBox = document.getElementById("input-box")
+// const button = document.getElementById("button");
+// const list = document.getElementById("list-container")
+
+// function addTask (){
+//     if(inputBox.value === ''){
+//         alert("ADD SOME TASK FIRST ");
+//     }else{
+//         const li = document.createElement("li")  // got new li element
+//         li.innerHTML = inputBox.value;  // in this add value of input
+//         // list.appendChild(inputBox.value);
+//         list.appendChild(li);   // appended it to child
+//     }
+// }
+
+
+
+
